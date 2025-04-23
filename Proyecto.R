639dec91fa152f1064a4aacc7c32a4f5d124231b
Diego Leiva <diego.leiva.reyes@hotmail.com>
2025-04-23 13:36:49 -0400

    Limpieza y gráfico de dataset listo ahora si

--- Proyecto.R
+++ Proyecto.R
@@ -1,2 +1,15 @@
 #Temperatura global en temporalidad mensual a lo largo de 2 siglos aproximadamente
 dataset = read.csv("monthly.csv")
+dataset <- dataset[ , !(names(dataset) %in% "Source")]
+
+# Convertir la columna 'Year' a fecha (suponiendo formato YYYY-MM)
+dataset$Year <- as.Date(paste0(dataset$Year, "-01"))
+
+# Graficar la temperatura en el tiempo
+plot(dataset$Year, dataset$Mean,
+     type = "l",                  # línea
+     col = "blue",
+     xlab = "Fecha",
+     ylab = "Temperatura media",
+     main = "Temperatura global mensual a lo largo del tiempo")
+
