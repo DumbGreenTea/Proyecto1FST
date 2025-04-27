90389305c3de607cfc3090506dfbadbed6a5b803
Diego Leiva <diego.leiva.reyes@hotmail.com>
2025-04-27 17:29:52 -0400

    Se realizan pruebas para responder las 3 primeras preguntas de análisis

--- Proyecto.R
+++ Proyecto.R
@@ -1,15 +1,86 @@
-#Temperatura global en temporalidad mensual a lo largo de 2 siglos aproximadamente
-dataset = read.csv("monthly.csv")
-dataset <- dataset[ , !(names(dataset) %in% "Source")]
+#===============================================#
+#           Importación de librerías 
+#===============================================#
+  # Instalación y carga de paquetes necesarios
+  install.packages("forecast")
+  install.packages("TTR")
+  install.packages("tseries")
+  install.packages("mFilter")
+  library(forecast)
+  library(TTR)
+  library(tseries)
+  library(mFilter)
+  
+#===============================================#
+#      Limpieza y Presentación del DataSet
+#===============================================#
+  # Importación del data set (serie de tiempo)
+  # Temperatura global en temporalidad mensual a lo largo de 2 siglos aproximadamente
+  dataset = read.csv("monthly.csv")
+  #Ver los primeros elementos del dataset
+  head(dataset)
+  
+  # Eliminar columna source (no es importante para el análisis de este trabajo)
+  dataset <- dataset[ , !(names(dataset) %in% "Source")] 
+  # Convertir la columna 'Year' a fecha (suponiendo formato YYYY-MM), manejamos el primer día de cada mes
+  dataset$Year <- as.Date(paste0(dataset$Year, "-01"))
+  
+  # Ver los primeros elementos del dataset sin columna Source
+  head(dataset)
+  
+  # Graficar la temperatura en el tiempo
+  plot(dataset$Year, dataset$Mean,
+       type = "l",                  # línea
+       col = "blue",
+       xlab = "Fecha",
+       ylab = "Temperatura media",
+       main = "Temperatura global mensual a lo largo del tiempo")
 
-# Convertir la columna 'Year' a fecha (suponiendo formato YYYY-MM)
-dataset$Year <- as.Date(paste0(dataset$Year, "-01"))
+  
+#===============================================#
+#             Análisis de la Serie 
+#===============================================#
+  # Transformar el dataset a un objeto de serie de tiempo (ts)
+  temp_ts <- ts(dataset$Mean, 
+                start = c(as.numeric(format(min(dataset$Year), "%Y")), 
+                          as.numeric(format(min(dataset$Year), "%m"))), 
+                frequency = 12)
 
-# Graficar la temperatura en el tiempo
-plot(dataset$Year, dataset$Mean,
-     type = "l",                  # línea
-     col = "blue",
-     xlab = "Fecha",
-     ylab = "Temperatura media",
-     main = "Temperatura global mensual a lo largo del tiempo")
+  
+#======= Análisis de tendencia y periodicidad ========#
+  
+  # Gráfica de la serie de tiempo
+  plot(temp_ts, main = "Serie de Tiempo de Temperatura Global", 
+       xlab = "Tiempo", ylab = "Temperatura Media")
+  
+  #Ya que nuestros datos ya son medias mensuales, buscaremos un promedio anual de temperaturas, pues las tº son variables al promedio, no acumulativas
+  aggregate(temp_ts, FUN = mean)
+  
+  # Pendiente (tendencia)
+  plot(aggregate(temp_ts, FUN = mean), main = "Tendencia Anual de Temperatura Global", 
+       xlab = "Año", ylab = "Temperatura Total Anual")
+  
+  # Periodicidad
+  boxplot(temp_ts ~ cycle(temp_ts), main = "Boxplot de Periodicidad de Temperatura",
+          xlab = "Mes", ylab = "Temperatura Media")
+  
+  
+#====== Tipo de Modelo ¿Aditivo o Multiplicativo ======#
+  
+  # Descomposición
+  decomp_add <- decompose(temp_ts, type = "additive")
+  plot(decomp_add)
+  
+  decomp_mult <- decompose(temp_ts, type = "multiplicative")
+  plot(decomp_mult)
+  
+#====== ¿Estacionaria débil? ======# ESTO ESTA EN PROCESO NO SE SI ESTÁ BIEN JSJSJSJ
+  acf(temp_ts, main = "ACF de la Temperatura Global")
+  pacf(temp_ts, main = "PACF de la Temperatura Global")
+  
+  library(tseries)
+  adf.test(temp_ts)$p.value
+  kpss.test(temp_ts)
+  
+
 
